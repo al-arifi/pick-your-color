a5177e4b3f4e9833d58cdf56db35fc569b5bf7bf
peas-developer <peasdeveloper@gmail.com>
2022-03-10 13:06:29 +0700

    la

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ addEventListener("click", (e) => {
   color.forEach((c) => {
     if (c == e.target) {
       body.className = "";
+      e.target.classList.add("active");
       body.classList.add(`${e.target.dataset.color}`);
     }
   });
